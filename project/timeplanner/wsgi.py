fbfc15ccd8e8c5fc731fe3c56b92228ba318e0b8
smallbath <mr.mukkuli@gmail.com>
2026-09-20 10:04:53 +0200

    Restructure: move Harpr to repo root, drop old version

--- project/timeplanner/wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI config for timeplanner project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timeplanner.settings")
-application = get_wsgi_application()
